88984ff3c770ddd219eae8baf883c22b7ebe0944
szk3600 <t-suzuki@world-works.co.jp>
2021-07-31 08:11:50 +0900

    update michinoeki

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,7 +13,7 @@
 
 <div class="header">
     <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
-       <a class="pure-menu-heading" href="">Your Site</a>
+       <a class="pure-menu-heading" href="#">Your Site</a>
 
         <ul class="pure-menu-list">
             <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Home</a></li>
