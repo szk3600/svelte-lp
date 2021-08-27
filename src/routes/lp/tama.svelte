1afb54ad0f7027e98a4b212dc06219cf6029f2ff
szk3600 <t-suzuki@world-works.co.jp>
2021-08-27 14:58:29 +0900

    tama added

---
+++ src/routes/lp/tama.svelte
@@ -0,0 +1,8 @@
+<script context="module">	import { browser, dev } from '$app/env';	export const hydrate = dev;	export const router = browser;	export const prerender = true;	import LP from '$lib/LP.svelte';</script>
+
+<script>
+	import lpProps from '$lib/data/lp/tama.json';
+</script>
+<LP {...lpProps} />
+
+
